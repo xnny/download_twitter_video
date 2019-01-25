9510ef233294d1da0ae6dd44d34f229ba30b236f
xnny <iwangtao@gmail.com>
2019-01-26 01:47:04 +0800

    添加下载;

--- js/background.js
+++ js/background.js
@@ -1,22 +1,10 @@
 /**
  * 背景页
  * User: wangtao
- * Date: 2019/1/16
+ * Date: 2019/1/25
  * Time: 20:43
  */
-/*
-// demo_0，发送消息
-chrome.runtime.onMessage.addListener(function (request, sender, callback) {
-    setTimeout(function () {
-        callback('hi');
-    }, 2000);
-    return true;
-});
-*/
-const __collection = ['twitter.com', 'www.instagram.com'];
-// const __api_url = 'https://mprss.read.mx/sys/config';
-const __api_url = 'http://127.0.0.1:8008/sys/config';
-const __domain_key = {'twitter.com': 6, 'www.instagram.com': 5};
+
 
 /**
  * 生成 JSON 文件
@@ -33,54 +21,72 @@ function generate_cookies_json(cookies_list) {
     return JSON.stringify(data_list);
 }
 
-function save_cookies(domain, json_data) {
-    let config_id = __domain_key[domain];
-    let form_data = new FormData();
-    form_data.append('option', config_id);
-    form_data.append('content', json_data);
-    let send_obj = {
-        'method': 'POST',
+function get_mp4_url(status_code, json_data) {
+    // let config_id = __domain_key[domain];
+    // let form_data = new FormData();
+    // form_data.append('option', config_id);
+    // form_data.append('content', json_data);
+    /*let send_obj = {
+        'method': 'GET',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
         'body': form_data
+    };*/
+    /**
+     :
+     ::
+
+     :
+     x-twitter-client-language: zh-tw
+     */
+
+    let header = {
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+        "x-csrf-token": '76b68f34a0b9956b639e5a4fe1ff54b2',
+        'x-twitter-active-user': 'yes',
+        'x-twitter-auth-type': 'OAuth2Session',
     };
+    let api_url = 'https://api.twitter.com/2/timeline/conversation/' + status_code + '.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&count=20&ext=mediaStats%2ChighlightedLabel%2CcameraMoment'
     fetch(
-        __api_url,
-        send_obj
-    ).then(
-        response=> console.log('Success:', response)
-    ).catch(error=> console.error('Error:', error));
+        api_url,
+        {
+            method: 'get',
+            headers: header
+        }
+    ).then(function (response) {
+        response.json().then(function(json_data) {
+            // JSON.globalObjects.tweets[1088636851110535168].extended_entities.media[0].video_info.variants[2].url
+            let mp4_url = json_data.globalObjects.tweets[status_code].extended_entities.media[0].video_info.variants[2].url;
+            chrome.downloads.download({
+                url: mp4_url,
+                conflictAction: 'uniquify',
+                saveAs: false
+            });
+        });
+        console.log(response.status)
+    }).catch(error=> console.error('Error:', error));
 }
 
-// demo_1 接收消息
-// chrome.runtime.addEventListener(
-chrome.runtime.onMessage.addListener(
-    function (request, sender, callback) {
-    let msg = request.content;
-    console.log('触发');
-    chrome.tabs.query(
-        { active: true },
-        function (tabs) {
-            let tab = tabs[0];
-            let url_obj = new URL(tab.url);
-            let domain = url_obj.hostname;
-            let index_int = __collection.indexOf(domain);
-            if (index_int < 0) {
-                return false;
-            }
 
-            let cookies_url = 'https://' + domain + '/';
-            console.log(cookies_url);
-            chrome.cookies.getAll(
-                {'url': cookies_url},
-                function (cookies) {
-                    let json_str = generate_cookies_json(cookies);
-                    console.log('json_str', json_str);
-                    save_cookies(domain, json_str);
-                }
-            );
+function main() {
+    let cookies_url = 'https://twitter.com/';
+    console.log(cookies_url);
+    chrome.cookies.getAll(
+        {'url': cookies_url},
+        function (cookies) {
+            let json_str = generate_cookies_json(cookies);
+            console.log('json_str', json_str);
+            get_mp4_url('1088636851110535168', '');
+            // save_cookies(domain, json_str);
         }
     );
+    alert('xx');
+}
 
-    callback(msg);
-    return true;
-});
+/**
+ * 监控点击事件
+ */
+chrome.browserAction.onClicked.addListener(main);
